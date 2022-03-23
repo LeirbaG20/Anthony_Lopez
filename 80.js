94ec90c98185a463056f86e5fcfdf51aa92ca947
LeirbaG20 <102021254+LeirbaG20@users.noreply.github.com>
2022-03-23 11:41:11 +0100

    new

--- 80.js
+++ 80.js
@@ -1,31 +1,51 @@
-let p1 = " ";   //Declaramos variable p1
+let p1;         //Declaramos variable p1
 p1 = 4;         //Asignamos variable p1
-let p2 = 3;     //Declaramos y asignamos p2
-// Declaramos la funcion indicando que hacer con los parametros recibidos
+let p2 = 3;     //Declaramos y Asignamos a la vez variable p2
+
+//Declaramos la función indicando que hacer con los parámetros recibidos
 function myFunction(p1, p2) {
-    let p3 = p1 * p2;
-    return p3;
+    let p3 = p1 * p2;             //Operamos con los parámetros
+    return p3;                    //Devolvemos el resultado
 }
-document.getElementById("demo").innerHTML = myFunction(4, 3);
-//Lamamos a la funcion y le pasamos como parametros las dos variables
-document.getElementById("demo3").innerHTML = myFunction2(p1, p2);
-//
-let p4 = myFunction(p1 * p2);
-//
+//Llamamos a la función y le pasamos como parámetro las dos variables
+//...y pasamos el resultado a otra variable:
+let p4 = myFunction(p1, p2);
+//Mostramos el resultado en una etiqueta del html con id = "demo"
+document.getElementById("demo").innerHTML = p4;
 
+myFunction1();
 
-//Array
-//Array es una variable que puede tener muchos valores distintos a la vez-
-let person = ["Joe", "Doe", 46];
-document.getElementById("demo3").innerHTML = person;
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-document.getElementById("demo8").innerHTML = fruits;
-//Cambiar el valor segun su posicion en la array
-fruits[0] = "Strawberry";
+function myFunction1() {
+    var carName = "Volvo";
 
-//Seleccionar o elimar valores
-const fruits = ["Banana", "Orange", "Lemon","Apple", "Mango"];
-const citrus=fruits.slice(1,3);//crea un array nuev con los elementos seleccionados
-document.getElementById("demo9").innerHTML) = fruits + "<br></br>"+ citrus;
-fruits.splice(3,1);//Modifica 
+    var queTipoDeDatoUtiliza = typeof carName;
 
+    document.getElementById("demo1").innerHTML = queTipoDeDatoUtiliza + " " + carName;
+}
+
+//Arrays
+//Array es una variable que puede tener muchos valores distintos a la vez
+let person = ["John", "Doe", 46];
+document.getElementById("demo2").innerHTML = typeof person[0] + " " + person[0];
+document.getElementById("demo3").innerHTML = typeof person[2] + " " + person[2];
+
+//Añadir Modificar valores
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+document.getElementById("demo4").innerHTML = fruits.toString();
+
+document.getElementById("demo5").innerHTML = fruits.length;
+
+fruits[5] = "Kiwi";
+document.getElementById("demo6").innerHTML = fruits.toString();
+
+fruits[fruits.length] = "Pear";
+document.getElementById("demo7").innerHTML = fruits.toString();
+
+fruits[4] = "Strawberry";
+document.getElementById("demo8").innerHTML = fruits.toString();
+
+//Seleccionar o eliminar valores
+const citrus = fruits.slice(1, 3); //crea un array nuevo con los elementos seleccionados
+document.getElementById("demo9").innerHTML = fruits + "<br><br>" + citrus;
+fruits.splice(3, 1); //MODIFICA el array eliminando estos elmentos
+document.getElementById("demo10").innerHTML = fruits;
