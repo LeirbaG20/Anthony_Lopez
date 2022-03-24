bd083d370830247314660b59ee3f3cc00f4912e8
LeirbaG20 <102021254+LeirbaG20@users.noreply.github.com>
2022-03-25 00:10:12 +0100

    new

---
+++ index.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("panel").style.display = "block";
+  }
+  function myFunctionhide() {
+    document.getElementById("panel").style.display = "none";
+  }
+  function myFunction2() {
+    document.getElementById("panel2").style.display = "block";
+  }
+  function myFunctionhide2() {
+    document.getElementById("panel2").style.display = "none";
+  }
+  function myFunction3() {
+    document.getElementById("panel3").style.display = "block";
+  }
+  function myFunctionhide3() {
+    document.getElementById("panel3").style.display = "none";
+  }
